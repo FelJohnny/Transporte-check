44526bc129e5f576b49dad0f1eb693d4a94f1bf9
Felipe Johnny <felipijohnny@outlook.com>
2023-07-21 18:55:50 -0300

    aplicando regras e logicas em todos veiculos

--- js/entrega.js
+++ js/entrega.js
@@ -2,12 +2,12 @@ function trocaVeiculo(){
     var veiculoList = document.getElementById("veiculos");
     var veiculosValues = veiculoList.options[veiculoList.selectedIndex].value;
     
-    let imgVeiculo1 = document.querySelector('.contain-circulo-fiorino');
-    let imgVeiculo2 = document.querySelector('.contain-circulo-hyundai-hr');
+    let imgVeiculo1 = document.querySelector('.contain-circulo-veiculo1');
+    let imgVeiculo2 = document.querySelector('.contain-circulo-veiculo2');
 
     if (veiculosValues == 1){
         
-        imgVeiculo1.style.display ='block';    
+        imgVeiculo1.style.display ='block';   
         imgVeiculo2.style.display='none';
         
     }else if(veiculosValues == 2){
