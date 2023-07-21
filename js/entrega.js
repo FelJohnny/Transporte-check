85a999d5482aab305424d7180853c6f8f1bacc03
Felipe Johnny <felipijohnny@outlook.com>
2023-07-21 19:26:23 -0300

    aplicando mais js

--- js/entrega.js
+++ js/entrega.js
@@ -1,18 +1,27 @@
 function trocaVeiculo(){
     var veiculoList = document.getElementById("veiculos");
+    var danificacaoList = document.getElementById("danificacao");
     var veiculosValues = veiculoList.options[veiculoList.selectedIndex].value;
+    let h3Danificacao = document.querySelector('.desc-danificacao');
     
+
     let imgVeiculo1 = document.querySelector('.contain-circulo-veiculo1');
     let imgVeiculo2 = document.querySelector('.contain-circulo-veiculo2');
 
+    
+
     if (veiculosValues == 1){
         
+        h3Danificacao.style.display='block';
+        danificacaoList.style.display='block';
+
         imgVeiculo1.style.display ='block';   
         imgVeiculo2.style.display='none';
         
-    }else if(veiculosValues == 2){
         
-
+    }else if(veiculosValues == 2){
+        h3Danificacao.style.display='block';
+        danificacaoList.style.display='block';
         imgVeiculo2.style.display='block';
         imgVeiculo1.style.display='none';
     }
